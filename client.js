063b913de75ee2243479ddb9e925fa525c12448e
Joao Vitor <joaovitorti07@gmail.com>
2023-03-27 16:30:02 -0300

    Aula 16.3. Entendendo o funcionamento básico de CORS e habilitando na API

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 function consultarRestaurantes() {
-    $.ajax({
-      url: "http://api.algafood.local:8080/restaurantes",
-      type: "get",
-  
-      success: function(response) {
-        $("#conteudo").text(response);
-      }
-    });
-  }
-  
-  $("#botao").click(consultarRestaurantes);
+  $.ajax({
+    url: "http://api.algafood.local:8080/restaurantes",
+    type: "get",
+
+    success: function(response) {
+      $("#conteudo").text(JSON.stringify(response));
+    }
+  });
+}
+
+$("#botao").click(consultarRestaurantes);
